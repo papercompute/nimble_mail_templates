24453a85c292b45c7f8129b71158c7d6e1d7f27e
alex <alex@localhost.localdomain>
2015-02-04 11:06:19 +0300

    deal view hack

--- nimble_deal_mail_templates.js
+++ nimble_deal_mail_templates.js
@@ -1,76 +1,94 @@
 function init() {
   var taistApi;
   
-/*
-
-<input type="text" class="nmbl-AdvancedTextBox" maxlength="256" placeholder="Name">
-
-
-
-
-*/
 
   var dmtcEl=null;
-  var userId=null;
-  var mailTemplates={A:[
+  var dmtocEl=null;
+
+  var mailTemplates=[
     {topic:"Hello log name topic template aa s s s df sd fsd fs df sd f sd fs d fs df s df s dffdf end",body:"Dear $contacts-lastname$!"},
     {topic:"Bye",body:"Dear $contacts-lastname$!  as da sd a sd a sd as d as da sd a sd a s da sd a sd as d"},
     {topic:"Ping",body:"Dear $contacts-lastname$!"}
-  ]};
+  ];
 
-  function saveToStorage(){
-   localStorage.setItem(userId,JSON.stringify(mailTemplates));
+
+const templateBody = 
+        'Dear $contacts-lastname$!\r\n\r\n'
+       +'Thanks for subscribing super CRM.\r\n'
+       +'We are so glad that you are now member of community.\r\n' 
+       +'Please feel free to revert to us if you need any assistance.\r\n\r\n'
+       +'Regards,\r\n'
+       +'$username$ ';
+
+
+  function saveToStorage(id){
+   localStorage.setItem(id,JSON.stringify(mailTemplates));
   }
 
-  function readFromStorage(){
-   var item=localStorage.getItem(userId);
-    //console.log('readFromStorage',item);
+  function readFromStorage(id){
+   var item=localStorage.getItem(id);
    if(item){
     mailTemplates=JSON.parse(item); 
    }
   }
 
-  function rebuildTemplates(){
-    var  sdvEl = document.querySelector(".SettingsDealsView");
-    console.log('rebuildTemplates');
+  function rebuildDealHack(piwEl){
+    //var  piwEl = document.querySelector(".profileInfoWrapper");
+    console.log('rebuildDealHack()',piwEl);
+    if(piwEl){
+     if (!document.querySelector(".dealMailToContainer")){
+
+      console.log('!!!.dealMailToContainer 000');
+
+      //piwEl.appendChild(dmtcEl);
+      var giEl=piwEl.querySelector(".generalInfo");
+      if(giEl){
+
+
+      dmtocEl = document.createElement("div");
+      dmtocEl.setAttribute('class', 'dealMailToContainer');
+      var html='<div class="dealMainField">'
+  	 +'<div style="">Hack:<div class="stage" hastooltip="true">'
+          +'<div class="gwt-HTML">Hack Makers</div>'
+          +'</div>'
+         +'</div>'
+        +'</div>';
+
+      dmtocEl.innerHTML=html;
+      giEl.appendChild(dmtocEl);
+      console.log('!!!.dealMailToContainer 111');
+      }
+
+     }
+    }
+  }
+
+  function rebuildTemplates(sdvEl){
+    //var  sdvEl = document.querySelector(".SettingsDealsView");
+    console.log('rebuildTemplates()',sdvEl);
     if(sdvEl){
     if (!document.querySelector(".dealMailTemplatesContainer")){
 
-      readFromStorage();
+      var userId=document.querySelector("a.userName");
+      if(userId)readFromStorage(userId+"_deal_mail_templates");
 
       dmtcEl = document.createElement("div");
       dmtcEl.setAttribute('class', 'dealMailTemplatesContainer');
       var html='<div class="subHeader">Mail templates</div>'
       +'<img class="amountHeaderInfo" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAACHElEQVR42pWVPUsDQRCGo4ZYKNhqZe1fELEQxMJ/4x8I1ulCQu4uMbl8cIFoUFt7vxo7wWChIFaijYJfl6zvG2fi5jyjLgy7t5l97p3ZmUsi8cNotVoTtOh+Op0ehyUTfx08YIOwTuXz+VnXdeey2exkBDw+EmaDHMdZBsTHfIH5XqwDqxeLxZW4M7EwqsChLQxTr9cNZ9u4Vy6XDcGZTGYqFqrSCYOi42azyQM92CvUYO28Yf1cKpW4/8rfgiAw8D2r1WpTNmNIHZURBscXrEMB3HmetwjwAtaXoi6kj0CDIaDCmDOGJMp6eH7H243nOQdWXsuNRoM+b1gT1q1UKqZQKKwJNGmr85kfOsMUbBy3f3ATtsGwofbrN3kp1q2oyhRvU8MRZyNg5uwWdqNA67eQUeH5SnPZH6wzbD4wZwxDnavVKucT9cN6l1FQmfh0Py/NfczlcvMDIPIQA3T+A3zC2S+g1F7HDlkV4vCpdSl7EWAoF3mNKKeHahBOjahzH+g5p8aYMVG4b/vopWBuD1ja6GwnUdjVspGQDy2F2wrk7fOlvu9zXh+Uja2S7WQXNmuMtw9bxfMS5iMBMC3PUtjtb52iD+xNtlPQDPRyWJM9XpbkSus0JAzzOWzmG9DuGNYT24nqmB+CWHs0rrknKncU9usXh4PtxA5g0bLOxK4ZouZsJOynDycVs2hZZ4PS+OsHNgJOxh347S/gAzHtmVNmDMRdAAAAAElFTkSuQmCC" hastooltip="true">';
 
-      if(mailTemplates.A && mailTemplates.A.length>0){
-//	html+='<div class="mailTemplatesList">';
-        //console.log('mailTemplates.A.length',mailTemplates.A.length);
-/*
-       for(var i=0;i<mailTemplates.A.length;i++){
-    	html+='<div class="mailTemplateWidget">'
-         +'<div class="mailTemplateViewContainer">'
-	  +'<div class="mailTemplateViewTopic">'+mailTemplates.A[i].topic+'</div>'
-          +'<div class="mailTemplateViewBody">'+mailTemplates.A[i].body+'</div>' 
-           +'<div class="hoverContainer"><a class="gwt-Anchor">Modify</a>'
-           +'<div style="clear:both"></div>'
-           +'</div>'
-         +'</div>'	
-         +'</div>'
-	+'<div style="clear:both"></div>';
-       }
-*/
+      if(mailTemplates && mailTemplates.length>0){
 
 html+='<div class="mailTemplatesList">';
 
-       for(var i=0;i<mailTemplates.A.length;i++){
+       for(var i=0;i<mailTemplates.length;i++){
        var topic;
-       if(mailTemplates.A[i].topic.length>20){topic=mailTemplates.A[i].topic.substring(0,20)+"..";}
-       else topic=mailTemplates.A[i].topic;
+       if(mailTemplates[i].topic.length>20){topic=mailTemplates[i].topic.substring(0,20)+"..";}
+       else topic=mailTemplates[i].topic;
 
        var body;
-       if(mailTemplates.A[i].body.length>30){body=mailTemplates.A[i].body.substring(0,30)+"..";}
-       else body=mailTemplates.A[i].body;
+       if(mailTemplates[i].body.length>30){body=mailTemplates[i].body.substring(0,30)+"..";}
+       else body=mailTemplates[i].body;
 
 html+='<div class="mailTemplateWidget">'
         +'<div class="mailTemplateViewContainer">'
@@ -100,12 +118,6 @@ html+='<div class="mailTemplateWidget">'
       //+'<div class="mail-temlate-label">Body:</div>' 
 
       +'<textarea class="nmbl-AdvancedTextArea" >'
-       +'Dear $contacts-lastname$!\r\n\r\n'
-       +'Thanks for subscribing super CRM.\r\n'
-       +'We are so glad that you are now member of community.\r\n' 
-       +'Please feel free to revert to us if you need any assistance.\r\n\r\n'
-       +'Regards,\r\n'
-       +'$username$ '
       +'</textarea>'
 
       +'<div class="buttonFooter">'
@@ -126,7 +138,9 @@ html+='<div class="mailTemplateWidget">'
 
       sdvEl.appendChild(dmtcEl);
 
-      userId=document.querySelector("a.userName");      
+      
+
+// Add Template
 
       var atEl=dmtcEl.querySelector("a.add-Mail-Template");
       if(atEl){
@@ -141,10 +155,12 @@ html+='<div class="mailTemplateWidget">'
         taistApi.log("a.add-Mail-Template");
       }
 
+// Save template
       var stEl=dmtcEl.querySelector("a.save-Mail-Template");
       if(stEl){
         stEl.onclick=function(){
-          taistApi.log("stEl.onclick");
+          var userId=document.querySelector("a.userName");      
+          taistApi.log("stEl.onclick");          
           if(userId){
             localStorage.setItem(userId, foo);
 
@@ -158,6 +174,7 @@ html+='<div class="mailTemplateWidget">'
       }
 
 
+// Cancel template
       var ctEl=dmtcEl.querySelector("a.cancel-Mail-Template");
       if(ctEl){
         ctEl.onclick=function(){
@@ -176,7 +193,8 @@ html+='<div class="mailTemplateWidget">'
 
 
 var mainPanelObserver=null;
-
+var SettingsDealsViewCounter=0;
+var profileInfoWrapperCounter=0;
 
 function prepareIt() {
 
@@ -203,12 +221,19 @@ if(bodyTarget){
        var nimbleMainPanel = document.querySelector("#main");
        if(nimbleMainPanel && !mainPanelObserver){ 
         mainPanelObserver=new MutationObserver(function(mutations) {
-          var settingsTarget = nimbleMainPanel.querySelector(".SettingsDealsView");
-          if(settingsTarget){
-           rebuildTemplates();
-           taistApi.log("mainPanelObserver.disconnect()");
-           mainPanelObserver.disconnect();
+          var SettingsDealsView = nimbleMainPanel.querySelector(".SettingsDealsView");
+          if(SettingsDealsView){
+           rebuildTemplates(SettingsDealsView);
+           taistApi.log("mainPanelObserver.disconnect() SettingsDealsView");
+           SettingsDealsViewCounter++;          
           }
+          var profileInfoWrapper = nimbleMainPanel.querySelector(".profileInfoWrapper");
+          if(profileInfoWrapper){           
+           rebuildDealHack(profileInfoWrapper);
+           taistApi.log("mainPanelObserver.disconnect() profileInfoWrapper();");
+           profileInfoWrapperCounter++;          
+          }
+          if(SettingsDealsViewCounter>0 && profileInfoWrapperCounter>0){mainPanelObserver.disconnect();}
         });
         mainPanelObserver.observe(nimbleMainPanel, { childList: true, subtree : true});
         taistApi.log("bodyObserver.disconnect()");
